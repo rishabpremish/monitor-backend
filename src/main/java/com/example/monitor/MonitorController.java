259332e434c79b1143097ca1bb5a0364ed7d90cd
Rishab Premish <rishabpremish@gmail.com>
2026-08-07 11:13:42 -0400

    Update monitor controller response status

--- src/main/java/com/example/monitor/MonitorController.java
+++ src/main/java/com/example/monitor/MonitorController.java
@@ -18,20 +18,20 @@ public class MonitorController {
    @GetMapping("/api/monitors")
    public ResponseEntity<ArrayList<Monitor>> getAllMonitors() throws SQLException {
       MonitorRepository repository = new MonitorRepository();
-      return ResponseEntity.status(201).body(repository.getAllMonitors());
+      return ResponseEntity.ok(repository.getAllMonitors());
    }
 
    @GetMapping("/api/monitors/{id}")
    public ResponseEntity<ArrayList<Monitor>> getMonitorById(@PathVariable int id) throws SQLException {
       MonitorRepository repository = new MonitorRepository();
-      return ResponseEntity.status(201).body(repository.getMonitorById(id));
+      return ResponseEntity.ok(repository.getMonitorById(id));
    }
 
    @DeleteMapping("/api/monitors/{id}")
    public ResponseEntity<String> deleteMonitorById(@PathVariable int id) throws SQLException {
       MonitorRepository repository = new MonitorRepository();
       repository.deleteMonitorById(id);
-      return ResponseEntity.status(201).body("Monitor successfully deleted");
+      return ResponseEntity.ok("Monitor successfully deleted");
    }
 
 }
